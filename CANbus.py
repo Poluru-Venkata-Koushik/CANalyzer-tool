9a2201313568e0b83ffba0e3d2ae094a9b364ecd
Poluru-Venkata-Koushik <75077495+Poluru-Venkata-Koushik@users.noreply.github.com>
2025-07-04 07:21:09 +0530

    Update CANbus.py

--- CANbus.py
+++ CANbus.py
@@ -11,6 +11,24 @@ class CANbus():
             self.ECUs.append(ecuIn)
             ecuIn.bus = self
         else:
-            pass
+            print("ECU already registered")
 
+    def sendmessage(self, messageIn):
+        heapq.heappush(self.messages, messageIn)
+        print(f"Message {messageIn.msgID} sent to bus")
 
+    def dispatchMessages(self):
+        while self.messages:
+            message = heapq.heappop(self.messages)
+            for ecu in self.ECUs:
+                if message.msgID in ecu.filter:
+                    ecu.receiveMessage(message)
+                else:
+                    print(f"Message {message.msgID} not filtered by ECU {ecu.name}")
+
+    def __str__(self):
+        return (
+            '''
+                This is the canbus class.
+            ''')
+    
