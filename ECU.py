04781da35a1a70bcf3d1dd4ecc560e59ae943186
Poluru-Venkata-Koushik <75077495+Poluru-Venkata-Koushik@users.noreply.github.com>
2025-07-04 18:52:50 +0530

    Update ECU.py

--- ECU.py
+++ ECU.py
@@ -6,11 +6,19 @@ class ECU():
         self.bus = None
         self.filter = []
     
-    def sendMessage(self, messageId, Data):
-        pass
+    def sendMessage(self, message):
+        self.bus.sendmessage(message)
 
-    def receiveMessage(self):
-        pass
+    def receiveMessage(self, message):
+        if  message.msgID in self.filter:
+            print (f'''
+                Message : {message.data}
+                ID : {message.msgID}
+        ''')
+        else:
+            print ("This is not supposed to be happening, Must have been filtered in the BUS itself")
+        
+
 
     def addFilter(self, ID):
         if ID not in self.filter:
